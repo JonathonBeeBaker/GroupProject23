891a7b3487041b90316c37216b61e2249e9822b2
Ethan Farmer <farmetha@gmail.com>
2019-02-09 21:47:58 -0700

    fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <DemoProductList />
+        <DemoProductList category="newparent" />
       </div>
     );
   }
