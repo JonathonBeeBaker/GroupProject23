84f0aac1e5f76fe8207c27331d1a10089a3d4131
Steve Merrell <archisteve@hotmail.com>
2019-02-09 18:43:37 -0700

    third commit, teens.js not complete yet

--- scraper/teens.js
+++ scraper/teens.js
@@ -1,63 +1,47 @@
-// scrape script
-// =============
-
 // Require axios and cheerio, making our scrapes possible
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// This function will scrape the NYTimes website
-var scrape = function() {
-  // Scrape the NYTimes website
-  return axios.get("http://www.zumies.com").then(function(res) {
+// This function will scrape the fat brain toys website
+var scrape = function(cb) {
+  // Scrape the fatbraintoys website
+  return axios.get("https://www.fatbraintoys.com/specials/top_sellers.cfm").then(function(res) {
     var $ = cheerio.load(res.data);
     console.log("scraping");
-    // Make an empty array to save our article info
-    var articles = [];
-
+    // Make an empty array to save our products info
+    var products = [];
     // Now, find and loop through each element that has the "css-180b3ld" class
     // (i.e, the section holding the articles)
-    $("div.css-1100km").each(function(i, element) {
+    $(".item").each(function(i, element) {
       // In each article section, we grab the child with the class story-heading
-
       // Then we grab the inner text of the this element and store it
       // to the head variable. This is the article headline
-      var head = $(this)
-        .find("h2")
+      var item = $(element)
+        .find(".product-catalog-label")
         .text()
         .trim();
-
-      // Grab the URL of the article
-      var url = $(this)
-        .find("a")
-        .attr("href");
-
-      // Then we grab any children with the class of summary and then grab it's inner text
-      // We store this to the sum variable. This is the article summary
-      var sum = $(this)
-        .find("p")
-        .text()
-        .trim();
-
+    var url = $(this).find('.product-catalog-label').parent('a').attr('href');
+    var img = $(this).find('img').attr('data-original');
+    var price = $(this).find('.iparPrice').text().trim();
+    console.log(img);
       // So long as our headline and sum and url aren't empty or undefined, do the following
-      if (head && sum && url) {
-        // This section uses regular expressions and the trim function to tidy our headlines and summaries
-        // We're removing extra lines, extra spacing, extra tabs, etc.. to increase to typographical cleanliness.
-        var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-        var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+      if (item) {
 
         // Initialize an object we will push to the articles array
 
         var dataToAdd = {
-          headline: headNeat,
-          summary: sumNeat,
-          url: "https://www.zumies.com" + url
+          item: item,
+          url: "https://www.fatbraintoys.com" + url,
+          image: img,
+          price: price
         };
 
-        articles.push(dataToAdd);
+        products.push(dataToAdd);
       }
     });
-    return articles;
-  });
+    //return products;
+    cb(products);
+  }).catch(err => console.log(err) && cb("err"));
 };
 
 // Export the function, so other files in our backend can use it
