4d3e56eaf8a2597ab3bf2663cbed2e833196abf3
JonathonBee <jonathonbeebaker@gmail.com>
2019-02-12 11:03:33 -0700

    Added react router, added Home component. router is kind of working.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,78 @@
 import React, { Component } from "react";
 import "./App.css";
+import Home from "./components/Home";
 import KidsProductList from "./components/KidsProductList";
 import TeensProductList from "./components/TeensProductList";
+import {BrowserRouter, Route} from "react-router-dom";
+import { Container, Row, Col, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav,
+  NavItem, NavLink, UncontrolledDropdown, DropdownToggle, 
+  DropdownMenu, DropdownItem, } from 'reactstrap';
 import { Button } from 'reactstrap';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>GIFTBOT</h1> 
+        <BrowserRouter>
+          <div>
+          <Navbar color="info" light expand="md">
+          <NavbarBrand href="/"><h1>GIFTBOT</h1></NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/savedSelections.js">Saved Selections</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>        
+        </Navbar>
+        <br />
+        <br />
+        <br />
+      
+          <Container>
+            <Row>
+                <Col>
+                <Button color="dark">KIDS</Button>{' '}
+                </Col>
+                <Col>
+                <Button color="dark">TEENS</Button>{' '}
+                </Col>
+                <Col>
+                <Button color="dark">ELDERLY</Button>{' '}
+                </Col>
+            </Row>
+          </Container>
           <br />
-          <h3>Easy gift ideas</h3>        
+          <Container>
+            <Row>
+              <Col>
+                <Route path="/" component={Home} />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+            <Route path="/kidsproductlist" component={KidsProductList} />
+              </Col>
+            </Row>
+          </Container>
           </div>
-            <br />
-            <br />
-            <br />
-            <Button className="btnDark" color="dark">KIDS</Button>{' '}
-            <Button className="btnDark" color="dark">TEENS</Button>{' '}
-            <Button className="btnDark" color="dark">ELDERLY</Button>{' '}
-            <br />
-            <br />
-            <br />
-        <KidsProductList category="kids" /> 
-        <TeensProductList category="teens" />
-      </div>
-      
-
-      
+         
+        </BrowserRouter>
     );
   }
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import { Container, Row, Col, } from 'reactstrap';
+   
+
+const Home = () => {
+    return (
+        <div>HOME</div>
+    )
+}
+
+
+export default Home;
