e0fd7d0692b46b6df4bfb4f278f0097e0540fdcc
JonathonBee <jonathonbeebaker@gmail.com>
2019-02-12 17:50:15 -0700

    styled pages

--- client/src/components/ElderlyProductList.js
+++ client/src/components/ElderlyProductList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import "./Style.css";
+import { Container, Row, Col,  } from 'reactstrap';
 
 
 class ElderlyProductList extends Component {
@@ -18,18 +19,23 @@ class ElderlyProductList extends Component {
     render() {
         return (
             
-            <div>
+            <Container>
+                <Row>
+                    <Col></Col>
+                    <Col>
                 
-                    {this.state.products.slice(0, 6).map((item, key) => (
-                        <a target="_blank" key={key} href={item.url}>
-                            <img src={item.image} key={key} />
-                            <div>{item.item}</div>
-                            <div>{item.category}</div>
-                            <div>{item.price}</div>
-                        </a>                             
-                    ))}
-                
-            </div>
+                        {this.state.products.slice(0, 6).map((item, key) => (
+                            <a target="_blank" key={key} href={item.url}>
+                                <img src={item.image} key={key} />
+                                <div>{item.item}</div>
+                                <div>{item.category}</div>
+                                <div>{item.price}</div>
+                            </a>                             
+                        ))}
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
             
         );
     }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ import { Container, Row, Col, } from 'reactstrap';
 
 const Home = () => {
     return (
-        <div>HOME</div>
+        <div><h3>Find the perfect gift</h3></div>
     )
 }
 
